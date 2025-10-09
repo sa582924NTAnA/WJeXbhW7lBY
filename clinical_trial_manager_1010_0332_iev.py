a545b467af68c75a5049396bc3a5b579ffc3478b
sa582924NTAnA <lashandrahinda@gmail.com>
2025-10-09 20:32:26 +0100

    Introduce new 临床试验管理 feature

---
+++ clinical_trial_manager_1010_0332_iev.py
@@ -0,0 +1,6 @@
+# 代码生成时间: 2025-10-10 03:32:25
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLineEdit, QLabel, QMessageBox
+from PyQt5.QtCore import Qt
+
+"
