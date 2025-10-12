db3bcb0761e9c1278d9ee02399627d1d5fc08ca5
sa582924NTAnA <lashandrahinda@gmail.com>
2025-10-12 20:39:23 +0100

    Build 触摸手势识别 using PYQT

---
+++ touch_gesture_recognizer_1013_0339_oad.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-10-13 03:39:21
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
+from PyQt5.QtCore import Qt, QEvent, QRectF
+from PyQt5.QtGui import QPainter
+
+"
